48a3d319f549f1e981f2aca63e176505c956fff5
edpe <ed@edperkins.co.uk>
2021-06-20 23:40:31 +0100

    create single PCBTrace agent

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,55 +1,53 @@
-let x = 40;
-let y = 40;
-let rectangles = [];
+let xPos;
+let yPos;
 
-class Rectangle {
-  constructor(xPos, yPos) {
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.xSize = 40;
-    this.ySize = 40;
-  }
+let yChange = -1; // begins life travelling up
+let xChange = 0; // begins life travelling straight up
 
-  display(x, y) {
-    noStroke();
-    fill(255);
-    rect(this.xPos, this.yPos, this.xSize, this.ySize);
-  }
-
-  shrinkX() {
-    if (this.xSize > 1) {
-      this.xSize = this.xSize - 1;
-    }
-  }
-
-  shrinkY() {
-    if (this.ySize > 1) {
-      this.ySize = this.ySize - 1;
-    }
-  }
-}
+let randNum;
 
 function setup() {
-  createCanvas(920, 920);
-  for (var t = 0; t < 6; t++) {
-    for (var i = 0; i < 6; i++) {
-      rectangles.push(new Rectangle(x, y));
-      x = x + 160;
-    }
-    y = y + 160;
-    x = 40;
-  }
+  createCanvas(500, 500);
+  background(220);
+  xPos = width / 2;
+  yPos = height / 2;
 }
-function draw() {
-  background(0);
-  for (var i = 0; i < 36; i++) {
-    rectangles[i].display();
-    if (rectangles[i].xSize > 1) {
-      rectangles[i].shrinkX();
-    }
 
-    if (rectangles[i].xSize <= 1) {
-      rectangles[i].shrinkY();
+// need a walker class
+// set initial direction on the class by setting xChange and yChange
+
+function changeDirection(number, changeProbability, direction) {
+  if (number < changeProbability) {
+    switch (direction) {
+      case "upRight":
+        xChange === 1 ? (xChange = 0) : (xChange = 1);
+        yChange = -1;
+        break;
+      case "upLeft":
+        xChange === -1 ? (xChange = 0) : (xChange = -1);
+        yChange = -1;
+        break;
     }
   }
 }
+
+function updatePosition() {
+  xPos = xPos + xChange;
+  yPos = yPos + yChange;
+}
+
+function draw() {
+  noStroke();
+  fill(255);
+  ellipse(xPos, yPos, 4, 4);
+
+  if (frameCount % 4 === 0) {
+    randNum = Math.floor(random(100));
+    changeDirection(randNum, 3, "upLeft");
+  }
+
+  updatePosition();
+}
+
+// set initial direction using function -  up/down/left/right/diagonalLeft/diagonalRight
+// function chooses switch statement based on direction argument
