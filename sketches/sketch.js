73d046c4d441cbe9f695a95bcb08067d45d98064
edpe <ed@edperkins.co.uk>
2021-06-22 11:17:31 +0100

    feat: add random colors, thickness and more traces

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,7 +1,6 @@
+let traces = [];
 
-let traces = []
-
-
+colors = ["#D8DCDB", "#3B4F61", "#32393E", "#FFFFFF"];
 
 class Tracer {
   constructor(xPos, yPos, xChange, yChange, type, changeProbability) {
@@ -14,53 +13,55 @@ class Tracer {
     this.initialYPos = yPos;
     this.changeProbability = changeProbability;
     this.changeRouteCounter = 0;
+    this.color = colors[Math.floor(random(4))];
+    this.thickness = Math.floor(random(4)) + 1;
   }
 
   changeDirection() {
-      if (frameCount % 4 === 0) {
-        let randNum = Math.floor(random(100));
-        if (randNum < this.changeProbability) {
-          this.changeRouteCounter++
-          switch (this.type) {
-            case "up":
-              this.xChange === 1 ? (this.xChange = 0) : (this.xChange = 1);
-              break;
+    if (frameCount % 5 === 0) {
+      let randNum = Math.floor(random(100));
+      if (randNum < this.changeProbability) {
+        this.changeRouteCounter++;
+        switch (this.type) {
+          case "up":
+            this.xChange === 1 ? (this.xChange = 0) : (this.xChange = 1);
+            break;
 
-            case "upRight":
-              this.xChange === 1 ? (this.xChange = 0) : (this.xChange = 1);
-              break;
-        
-            case "right":
-              this.yChange === 1 ? (this.yChange = 0) : (this.yChange = 1);
-              break;
-          
-            case "downRight":
-              this.xChange === 1 ? (this.xChange = 0) : (this.xChange = 1);
-              break;
-          
-            case "down":
-              this.xChange === 1 ? (this.xChange = 0) : (this.xChange = 1);
-              break;
-          
-            case "downLeft":
-              this.xChange === -1 ? (this.xChange = 0) : (this.xChange = -1);
-              break;
-          
-            case "left":
-              this.yChange === 0 ? (this.yChange = -1) : (this.yChange = 0);
-              break;
-          
-            case "upLeft":
-              this.xChange === -1 ? (this.xChange = 0) : (this.xChange = -1);
-              break;
-          }
+          case "upRight":
+            this.xChange === 1 ? (this.xChange = 0) : (this.xChange = 1);
+            break;
+
+          case "right":
+            this.yChange === 1 ? (this.yChange = 0) : (this.yChange = 1);
+            break;
+
+          case "downRight":
+            this.xChange === 1 ? (this.xChange = 0) : (this.xChange = 1);
+            break;
+
+          case "down":
+            this.xChange === 1 ? (this.xChange = 0) : (this.xChange = 1);
+            break;
+
+          case "downLeft":
+            this.xChange === -1 ? (this.xChange = 0) : (this.xChange = -1);
+            break;
+
+          case "left":
+            this.yChange === 0 ? (this.yChange = -1) : (this.yChange = 0);
+            break;
+
+          case "upLeft":
+            this.xChange === -1 ? (this.xChange = 0) : (this.xChange = -1);
+            break;
         }
-      
+      }
     }
   }
 
   updatePosition() {
-    if (this.changeRouteCounter < 3) {
+    // todo prevent trace going off screen by adding or condition to stop updating if y or x position gets near windowWidth/height or 0
+    if (this.changeRouteCounter < 4) {
       this.xPos = this.xPos + this.xChange;
       this.yPos = this.yPos + this.yChange;
     }
@@ -68,48 +69,126 @@ class Tracer {
 
   displayTrace() {
     noStroke();
-    fill(255);
+    fill(this.color);
     ellipseMode(RADIUS);
-    ellipse(this.xPos, this.yPos, 2, 2);
-    ellipse(this.initialXPos, this.initialYPos, 4, 4);
-    if (this.changeRouteCounter >= 3) {
-    ellipse(this.xPos, this.yPos, 4, 4);
-
+    ellipse(this.xPos, this.yPos, this.thickness, this.thickness);
+    ellipse(
+      this.initialXPos,
+      this.initialYPos,
+      this.thickness * 2,
+      this.thickness * 2
+    );
+    // todo prevent trace going off screen by adding or condition to stop updating if y or x position gets near windowWidth/height or 0
+    if (this.changeRouteCounter >= 4) {
+      ellipse(this.xPos, this.yPos, this.thickness * 2, this.thickness * 2);
     }
-
   }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(220);
-  let traceTypes =
-  [
-    { xPos: windowWidth / 2, yPos: windowHeight / 2, xChange: 0, yChange: -1, type: "up", changeProbability: 5 },
-    { xPos: windowWidth / 2 + 30, yPos: windowHeight / 2, xChange: 1, yChange: -1, type: "upRight", changeProbability: 5 },
-    { xPos: windowWidth / 2 + 30, yPos: windowHeight / 2 + 30, xChange: 1, yChange: 0, type: "right", changeProbability: 5 },
-    { xPos: windowWidth / 2 + 30, yPos: windowHeight / 2 + 60, xChange: 1, yChange: 1, type: "downRight", changeProbability: 5 },
-    { xPos: windowWidth / 2, yPos: windowHeight / 2 + 60, xChange: 0, yChange: 1, type: "down", changeProbability: 5 },
-    { xPos: windowWidth / 2 - 30, yPos: windowHeight / 2 + 60, xChange: -1, yChange: 1, type: "downLeft", changeProbability: 5 },
-      {
-        xPos: windowWidth / 2 - 30, yPos: windowHeight / 2 +
-          30, xChange: -1, yChange: 0, type: "left", changeProbability: 5
-      },
-    { xPos: windowWidth / 2 -30 , yPos: windowHeight / 2, xChange: -1, yChange: -1, type: "upLeft", changeProbability: 5 },
-  ]
-   traces = traceTypes.map(trace => 
-    new Tracer(trace.xPos, trace.yPos, trace.xChange, trace.yChange, trace.type, trace.changeProbability)
-   )
+  background("#E1E1DF");
+  let traceTypes = [
+    {
+      xPos: windowWidth / 2,
+      yPos: windowHeight / 2,
+      xChange: 0,
+      yChange: -1,
+      type: "up",
+      changeProbability: 5,
+    },
+    {
+      xPos: windowWidth / 2 + 30,
+      yPos: windowHeight / 2,
+      xChange: 0,
+      yChange: -1,
+      type: "up",
+      changeProbability: 5,
+    },
+    {
+      xPos: windowWidth / 2 + 60,
+      yPos: windowHeight / 2,
+      xChange: 1,
+      yChange: -1,
+      type: "upRight",
+      changeProbability: 5,
+    },
+    {
+      xPos: windowWidth / 2 + 60,
+      yPos: windowHeight / 2 + 30,
+      xChange: 1,
+      yChange: 0,
+      type: "right",
+      changeProbability: 5,
+    },
+    {
+      xPos: windowWidth / 2 + 60,
+      yPos: windowHeight / 2 + 60,
+      xChange: 1,
+      yChange: 1,
+      type: "downRight",
+      changeProbability: 5,
+    },
+    {
+      xPos: windowWidth / 2 + 30,
+      yPos: windowHeight / 2 + 60,
+      xChange: 0,
+      yChange: 1,
+      type: "down",
+      changeProbability: 5,
+    },
+    {
+      xPos: windowWidth / 2,
+      yPos: windowHeight / 2 + 60,
+      xChange: 0,
+      yChange: 1,
+      type: "down",
+      changeProbability: 5,
+    },
+    {
+      xPos: windowWidth / 2 - 30,
+      yPos: windowHeight / 2 + 60,
+      xChange: -1,
+      yChange: 1,
+      type: "downLeft",
+      changeProbability: 5,
+    },
+    {
+      xPos: windowWidth / 2 - 30,
+      yPos: windowHeight / 2 + 30,
+      xChange: -1,
+      yChange: 0,
+      type: "left",
+      changeProbability: 5,
+    },
+    {
+      xPos: windowWidth / 2 - 30,
+      yPos: windowHeight / 2,
+      xChange: -1,
+      yChange: -1,
+      type: "upLeft",
+      changeProbability: 5,
+    },
+  ];
+  traces = traceTypes.map(
+    (trace) =>
+      new Tracer(
+        trace.xPos,
+        trace.yPos,
+        trace.xChange,
+        trace.yChange,
+        trace.type,
+        trace.changeProbability
+      )
+  );
 }
 
 function draw() {
   traces.forEach((trace) => {
     trace.displayTrace();
-      trace.changeDirection();
-      trace.updatePosition();
-    
-    }
-  )
+    trace.changeDirection();
+    trace.updatePosition();
+  });
 }
 
-
+//todo make sure traces don't cross
